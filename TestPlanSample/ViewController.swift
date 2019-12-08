ab6d836e51dbf27cf1d267b4b1cad858611ad7fc
akatsuki174 <dev2015js@gmail.com>
2019-12-08 13:37:49 +0900

    localized

--- TestPlanSample/ViewController.swift
+++ TestPlanSample/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.text = NSLocalizedString("Top_title_label", comment: "")
     }
 
 
