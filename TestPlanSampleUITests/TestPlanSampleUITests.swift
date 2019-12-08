1debd28f349bb48d56f623bd3d91e6b0f9fe204e
akatsuki174 <dev2015js@gmail.com>
2019-12-08 14:17:03 +0900

    add localizing test

--- TestPlanSampleUITests/TestPlanSampleUITests.swift
+++ TestPlanSampleUITests/TestPlanSampleUITests.swift
@@ -9,35 +9,36 @@
 import XCTest
 
 class TestPlanSampleUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
+    func testLabelLocalization() {
+        XCTAssertEqual(app.staticTexts["greetingLabel"].label, localizedString(key: "Top_title_label"))
+    }
+    
+    var currentLanguage: (langCode: String, localeCode: String)? {
+        let currentLocale = Locale(identifier: Locale.preferredLanguages.first!)
+        guard let langCode = currentLocale.languageCode else {
+            return nil
         }
+        var localeCode = langCode
+        if let scriptCode = currentLocale.scriptCode {
+            localeCode = "\(langCode)-\(scriptCode)"
+        } else if let regionCode = currentLocale.regionCode {
+            localeCode = "\(langCode)-\(regionCode)"
+        }
+        return (langCode, localeCode)
+    }
+
+    func localizedString(key:String) -> String {
+        let localizationBundle = Bundle(path: Bundle(for: TestPlanSampleUITests.self).path(forResource: currentLanguage?.langCode, ofType: "lproj") ?? "")
+        let result = NSLocalizedString(key, bundle:localizationBundle!, comment: "")        
+        return result
     }
 }
